9a94b91a08e319cf8043d369f8d57f13dd427c24
tB1ack <31832634+tB1ack@users.noreply.github.com>
2018-02-11 18:44:22 -0800

    Add files via upload

---
+++ challengeSimpleDarts/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace challengeSimpleDarts
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            Game game = new Game("Frank", "Jim");
+            string result = game.Play();
+            resultLabel.Text = result;
+        }
+    }
+}
